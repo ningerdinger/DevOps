1bfed31c018de20fe6b6ae502731c9f0f4846a15
ningerdinger <52888356+ningerdinger@users.noreply.github.com>
2024-12-07 16:03:40 +0100

    Add files via upload

--- main.tf
+++ main.tf
@@ -3,29 +3,7 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-test1"
   location = "West Europe"
 }
-
-resource "azurerm_storage_account" "storage_acc" {
-  name                     = "examplestoracc"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-# resource "azurerm_storage_container" "container" {
-#   name                  = "content"
-#   storage_account_id    = azurerm_storage_account.storage_acc.name
-#   container_access_type = "private"
-# }
-
-# resource "azurerm_storage_blob" "example" {
-#   name                   = "my-awesome-content.zip"
-#   storage_account_name   = azurerm_storage_account.storage_acc.name
-#   storage_container_name = azurerm_storage_container.container.name
-#   type                   = "Block"
-#   source                 = "some-local-file.zip"
-# }
